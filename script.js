e2186b6b0fda5d3760883d61c87dd4a839e89019
Arman Niyaj Mogal <100214997+ArmanMogal@users.noreply.github.com>
2022-11-21 21:47:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+//Write a code to add the even numbers and subract the odd numbers
+
+
+// var numsArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// var sum = 100;
+// for (var i = 0; i < numsArr.length; i++) {
+//     if (numsArr[i] % 2 != 0)
+//     {
+//         sum += numsArr[i];
+//     }
+//  else
+//     {
+//         sum = sum - numsArr[i];
+//     }
+// }
+// console.log(sum);
+
+
+
+
+
+// let a = prompt("First number?");
+// let b = prompt("Second number?");
+// alert(a + b);
+
+// let a = prompt("10");
+// let b = prompt("5");
+// console.log(alert(a + b));   
+
+// var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// for (var i = 0; i <10; i++) {
+//  if(numsArr[i] %2 == 0 )
+//  {
+//  numsArr[i] = even;
+//  }
+// }
+// console.log(numsArr);
+
+// var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// var sum=0;
+// for (var i = 0; i < numsArr.length; i++) {
+//  sum += numsArr[i];
+
+// }
+// console.log(sum);
+
+///////////Write a code to add the even numbers only
+
+// var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// var sum=0;
+// for (var i = 0; i <10; i++) {
+//  if(numsArr[i]%2==0);
+//  sum += numsArr[i]
+// }
+// console.log(sum);
+
+
+///Write a code to print inner arrays
+
+// var numsArr = [[1, 2, 3, 4, 5],[ 6, 7, 8, 9, 10, 11]];
+// for (var i = 0; i < numsArr.length; i++); {
+//  console.log( numsArr[i])
+// }
+
+// var numsArr = [[1, 2, 3, 4, 5],[ 6, 7, 8, 9, 10, 11]];
+// var sum_odd=0;
+// var sum_even=0;
+// for (var i = 0; i < numsArr.length; i++) {
+//  var inner_array = numsArr[i];
+//  for(var j = 0 ; j < inner_array.length;j++ ){
+//  if(numsArr[i]%2!=0)
+//  {
+//  sum_odd += numsArr[i]
+//  }
+//  else
+//  {
+//  sum_even += numsArr[i]
+//  }
+// }
+// }
+// console.log(sum_odd);
+// console.log(sum_even);
